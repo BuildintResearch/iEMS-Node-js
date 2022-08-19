496bff8dcc9f1d0bc1f15a024be0463c0d5b65eb
Lovlesh <research@buildint.co>
2022-08-19 19:23:36 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var bodyParser = require("body-parser");
 const app = express()
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodie
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodie
 
 app.post("/", function(req, res) {
   res.send(req.body);
